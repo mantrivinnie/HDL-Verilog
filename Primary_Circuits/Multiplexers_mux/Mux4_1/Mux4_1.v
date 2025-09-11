459fbc12b2da00d498d99b9f0f07cf2848480ec9
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-11 23:32:38 +0530

    Update Mux4_1.v

--- Primary_Circuits/Multiplexers_mux/Mux4_1/Mux4_1.v
+++ Primary_Circuits/Multiplexers_mux/Mux4_1/Mux4_1.v
@@ -18,6 +18,19 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+/*
+4:1 Multiplexer (MUX)
+A multiplexer (MUX) is a digital circuit that selects one of several inputs and forwards it to a single output line.
+A 4:1 multiplexer has:
+4 data inputs: 𝐼0,𝐼1,𝐼2,𝐼3​
+2 select lines: 𝑆1,𝑆0
+1 output: Y
+
+The select lines determine which input is connected to the output.
+
+Y=S1′​S0′​I0​+S1′​S0​I1​+S1​S0′​I2​+S1​S0​I3​
+*/
+
 
 module Mux4_1(Y,S,A);
 input [3:0]A;
