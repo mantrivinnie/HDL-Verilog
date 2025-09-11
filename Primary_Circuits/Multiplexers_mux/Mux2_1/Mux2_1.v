fc543d46339ac4aed9c60e89d9e7f5d443bffc36
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-11 23:28:28 +0530

    Update Mux2_1.v

--- Primary_Circuits/Multiplexers_mux/Mux2_1/Mux2_1.v
+++ Primary_Circuits/Multiplexers_mux/Mux2_1/Mux2_1.v
@@ -18,6 +18,18 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+/*
+2:1 Multiplexer (MUX)
+A multiplexer (MUX) is a combinational circuit that selects one input from multiple inputs and forwards it to a single output line.
+A 2:1 multiplexer has two data inputs (I0, I1), one select input (S), and one output (Y).
+The select line (S) decides which input is passed to the output:
+If S = 0, output Y = I0
+If S = 1, output Y = I1
+
+Y=S′I0+SI1
+*/
+
+
 
 module Mux2_1(
 input A, B, S, 
