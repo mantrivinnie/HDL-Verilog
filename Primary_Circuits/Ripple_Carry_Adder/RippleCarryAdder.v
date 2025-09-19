a9e310ab6bf1b94faa778539534e4d52bfd019b9
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-19 22:04:43 +0530

    Update RippleCarryAdder.v

--- Primary_Circuits/Ripple_Carry_Adder/RippleCarryAdder.v
+++ Primary_Circuits/Ripple_Carry_Adder/RippleCarryAdder.v
@@ -9,3 +9,18 @@ module Full_Adder(
     assign Cout = (A & B) | (Cin & (A ^ B));
 
 endmodule
+
+
+module Ripple_Carry_Adder(
+    input [3:0] A,
+    input [3:0] B,
+    input Cin,
+    output [3:0] S,
+    output Cout
+    );
+    wire [2:0]C;
+    Full_Adder FA1(A[0],B[0],Cin,S[0],C[0]),
+               FA2(A[1],B[1],C[0],S[1],C[1]),
+               FA3(A[2],B[2],C[1],S[2],C[2]),
+               FA4(A[3],B[3],C[2],S[3],Cout);
+endmodule
