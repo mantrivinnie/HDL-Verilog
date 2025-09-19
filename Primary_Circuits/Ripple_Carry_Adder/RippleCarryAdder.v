dccbdec951f0ce990795fff95137e73c21222aa5
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-19 22:04:04 +0530

    Create RippleCarryAdder file

---
+++ Primary_Circuits/Ripple_Carry_Adder/RippleCarryAdder.v
@@ -0,0 +1,11 @@
+module Full_Adder(
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+    );
+    assign Sum = A ^ B ^ Cin;
+    assign Cout = (A & B) | (Cin & (A ^ B));
+
+endmodule
