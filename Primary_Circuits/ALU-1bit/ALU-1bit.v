b2ebbadf02f69653d3ecb8d0cc72167f123c6a5c
Vinisha Mantri <mantrivinnie@gmail.com>
2025-09-26 21:48:16 +0530

    Create ALU-1bit file

---
+++ Primary_Circuits/ALU-1bit/ALU-1bit.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name: ALU_1bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_1bit(A,B,sel,result,carry);
+input A,B;
+input [2:0]sel;
+output reg result,carry;
+always @(*)
+begin
+    carry=0;
+    case(sel)
+        3'b000: {carry,result}=A+B;
+        3'b001: {carry,result}=A-B;
+        3'b010: result=A|B;
+        3'b011: result=A&B;
+        3'b100: result=A<<1;
+        3'b101: result=A>>1;
+        3'b110: result=A+1;
+        3'b111: result=A-1;
+        default: result=0;
+        endcase
+end
+endmodule
